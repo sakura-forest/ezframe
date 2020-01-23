0748ce5fa3873479394fd452616d7d43e7238f5d
KAMACHI Masayuki <mkamachi@gmail.com>
2020-01-23 11:20:07 +0900

    exampleを削除

--- example/todo/asset/js/mydatatable.js
+++
@@ -1,9 +0,0 @@
-function init_datatable(table) {
-  var datatable = new DataTable(table, {
-    pageSize: 3,
-    sort: [true, true],
-    filters: [false, true],
-    filterText: 'Type to filter... ',
-    pagingDivSelector: "#paging-first-datatable"
-  })
-}
