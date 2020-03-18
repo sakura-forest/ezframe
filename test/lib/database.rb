7e305823b60bfd32b019cecfec2420acdc9a07ce
KAMACHI Masayuki <mkamachi@gmail.com>
2020-03-18 16:42:08 +0900

    Databaseの構造の変更

--- test/lib/database.rb
+++ test/lib/database.rb
@@ -3,22 +3,35 @@
 require_relative "../test_helper.rb"
 
 class ColumnTypeTest < GenericTest
-  def test_basic_table_management
-    db_file = "db/test.sqlite"
-    db = Model::Database.new(db_file)
-    db.connect
-    db.drop_table?
-    db.create_table(table_name, { v1: "int", v2: "string" })
-    100.times do |i|
-      db.insert(table_name, { v1: i.to_s, v2: i })
-    end  
+  include Ezframe
 
-    dataset = db.dataset(table_name)
+  def test_basic_table_management
+    basic_test(use_connnection_pool: true)
+  end
+
+  def test_use_connection_pool
+    basic_test(use_connnection_pool: true)
+  end
+
+  def basic_test(opts = {})
+    db_file = "db/test.sqlite"
+    db_url = "sqlite://#{db_file}"
+    File.unlink(db_file) if File.exist?(db_file)
+    DB.init(db_url, use_connnection_pool: opts[:use_connnection_pool])
+    DB.connect
+    table_name = "test_table"
+    DB.create_table(table_name, { v1: "int", v2: "string" })
+    100.times do |i|
+      DB.insert(table_name, { v1: i.to_s, v2: i })
+    end
+
+    dataset = DB.dataset(table_name)
 
     data_a = dataset.where{ v1 >= 90 }.all
     assert_equal(10, data_a.length)
 
     data_a = dataset.all
     assert_equal(100, data_a.length)
+    DB.disconnect
   end
 end
